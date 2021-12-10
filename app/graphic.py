a7a37218cac8d0dc304aa85deb2c4bfbc038ab67
emelinevanderbeken <emelinevanderbeken@gmail.com>
2021-12-10 12:14:27 +0100

    Added options layout to setup_bar_chart

--- app/graphic.py
+++ app/graphic.py
@@ -1,6 +1,17 @@
 import plotly.graph_objects as go
 
-def setup_bar_chart(df, x_col, y_cols):
+def setup_bar_chart(df, x_col, y_cols, title):
     bar = [go.Bar(x=df[x_col], y=df[col], name=col) for col in y_cols]
     fig = go.Figure(bar)
+    fig.update_layout(
+        title=title,
+        xaxis_title="Months",
+        yaxis_title="Consumption",
+        legend_title="Legend",
+        font=dict(
+            family="Courier New, monospace",
+            size=18,
+            color="White"
+        )
+    )
     return fig

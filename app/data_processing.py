7887f1d407251105398246539c4eda52da7c29db
emelinevanderbeken <emelinevanderbeken@gmail.com>
2021-12-10 15:43:25 +0100

    Defined create_dataset

--- app/data_processing.py
+++ app/data_processing.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-cols = ['date', 'day_elec', 'night_elec', 'gas', 'water']
-data = ([] for col in cols)
-dic = {col: val for col, val in zip(cols, data)}
-df = pd.DataFrame(dic)
-df.to_csv('data.csv', index=False)
-
+def create_dataset(cols, filename):
+    data = ([] for col in cols)
+    dic = {col: val for col, val in zip(cols, data)}
+    df = pd.DataFrame(dic)
+    filepath = 'data_' + filename + '.csv'
+    df.to_csv(filepath, index=False)

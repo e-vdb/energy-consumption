93ee1136f89025ea7adcd59261117c56e21b6cde
emelinevanderbeken <emelinevanderbeken@gmail.com>
2021-12-14 08:20:57 +0100

    Defined class to load dataset from selectbox

--- app/frontend.py
+++ app/frontend.py
@@ -5,19 +5,24 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from graphic import setup_bar_chart
 from data_processing import find_csv_filenames, create_dataset
-
-
 st.set_page_config(layout="wide")
-list_years = [(datetime.today() + relativedelta(years=-i)).year for i in range(5)]
-dataCointe = Dataset('data_Cointe.csv')
-dataCointe.load()
+
 
 def load_file():
-    filenames = find_csv_filenames("/home/emeline/PycharmProjects/energy-consumption")
-    file = st.selectbox(
-        'Files',
-        filenames)
-    return file
+    return find_csv_filenames("/home/emeline/PycharmProjects/energy-consumption")
+
+
+
+list_years = [(datetime.today() + relativedelta(years=-i)).year for i in range(5)]
+filenames = load_file()
+
+class App:
+    def __init__(self, filepath):
+        self.fetch_data(filepath)
+
+    def fetch_data(self, filepath):
+        self.data = Dataset(filepath)
+        self.data.load()
 
 # Set the app title
 st.title("Energy consumption")
@@ -47,42 +52,43 @@ with expander:
             cols.append('water')
         submit_create = st.form_submit_button(label="Create")
 if submit_create:
-    filenames = find_csv_filenames("/home/emeline/PycharmProjects/energy-consumption")
+
     filepath = 'data_' + title + '.csv'
     if filepath in filenames:
         st.write('This files already exists. Enter a new location.')
     else:
         create_dataset(cols, title)
         st.write('Your file has been successfully created.')
+        filenames = load_file()
+
+file = st.selectbox('Files', filenames)
+dataset = App(file).data
+
+
 st.header('Fill your index')
 form = st.form(key="my_form", clear_on_submit = True)
 with form:
     d = st.date_input(
         "Date",
         value=pd.to_datetime("2021-12-01", format="%Y-%m-%d"))
-    day_rec = st.number_input('Electricity index (day)')
-    night_rec = st.number_input('Electricity index (night)')
-    rec_gas = st.number_input('Gas index')
-    rec_water = st.number_input('Water index')
+    saved_cols = list(dataset.saved_columns)[1:]
+    data_input = [st.number_input(col) for col in saved_cols]
     submit = st.form_submit_button(label="Add")
 
 if submit:
-    data = [d, day_rec, night_rec, rec_gas, rec_water]
+    data = [d] + data_input
     newline = {col: val for col, val in zip(cols, data)}
-    dataCointe.add(newline)
+    dataset.add(newline)
 
 st.header('Plot consumption')
 form_visual = st.form(key="my_form_visual", clear_on_submit=True)
 with form_visual:
-    file = load_file()
     option = st.selectbox(
         'Year',
         list_years)
     submit_see = st.form_submit_button(label="Show consumption plots")
 
 if submit_see:
-    dataset = Dataset(file)
-    dataset.load()
     dataset.evaluate_consumption()
     saved_cols = list(dataset.saved_columns)[1:]
     cols = dataset.consumption_columns[1:]
